028a462f8e89a769d07d311474d4ebaa8411d12b
Kautsar Al Bana <kautsar.al.bana@kineticastudios.com>
2024-03-06 16:13:10 +0700

    Update Simple Player Page

--- example/lib/examples/simple_player.dart
+++ example/lib/examples/simple_player.dart
@@ -15,16 +15,9 @@ class _SimplePlayerState extends State<SimplePlayer> {
   void initState() {
     playerController = BccmPlayerController(
       MediaItem(
-        url: 'https://h.m.cdn.klikfilm.net/premiere_monster/mp4dash/stream_widevine.mpd',
-        mimeType: 'application/dash+xml',
+        url: 'https://devstreaming-cdn.apple.com/videos/streaming/examples/adv_dv_atmos/main.m3u8',
+        mimeType: 'application/x-mpegURL',
         metadata: MediaMetadata(title: 'Apple advanced (HLS/HDR)'),
-        drm: MediaDrmConfiguration(
-          licenseUrl: 'https://drm-widevine-licensing.axprod.net/AcquireLicense',
-          headers: {
-            'X-AxDRM-Message':
-                'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ2ZXJzaW9uIjoxLCJjb21fa2V5X2lkIjoiMTc0MzNiZmUtZGQ0OS00ZGU3LWE5ZjQtYTgwNDAwNjEwYjE2IiwiYmVnaW5fZGF0ZSI6IjIwMjQtMDMtMDVUMTU6Mzg6MDcrMDc6MDAiLCJleHBpcmF0aW9uX2RhdGUiOiIyMDI0LTAzLTA5VDE1OjM4OjA3KzA3OjAwIiwibWVzc2FnZSI6eyJ0eXBlIjoiZW50aXRsZW1lbnRfbWVzc2FnZSIsInZlcnNpb24iOjIsImxpY2Vuc2UiOnsic3RhcnRfZGF0ZXRpbWUiOiIyMDI0LTAzLTA1VDE1OjM4OjA3KzA3OjAwIiwiZXhwaXJhdGlvbl9kYXRldGltZSI6IjIwMjQtMDMtMDlUMTU6Mzg6MDcrMDc6MDAiLCJhbGxvd19wZXJzaXN0ZW5jZSI6dHJ1ZX0sImNvbnRlbnRfa2V5c19zb3VyY2UiOnsiaW5saW5lIjpbeyJpZCI6IjdlM2E1N2IwLTgyYWMtNDM5ZC04Yzk4LTk2MjYwN2U3NTQ1NSIsIml2IjoiWlVsUTNMc0daNmVWeHFibXRzaUV2Zz09In1dfX19.6vnY_V5FOX0eH9dH4OMGVMTedVBikX68PJEjqFFyHCM'
-          },
-        ),
       ),
     );
     playerController.initialize().then((_) => playerController.setMixWithOthers(true)); // if you want to play together with other videos
