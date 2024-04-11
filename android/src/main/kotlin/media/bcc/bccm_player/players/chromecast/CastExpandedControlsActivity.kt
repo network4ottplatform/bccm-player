e0d742298194374118d1e5147cb72ca3be54d160
network4ottplatform <113431168+network4ottplatform@users.noreply.github.com>
2024-04-12 00:40:26 +0200

    Update CastExpandedControlsActivity.kt

--- android/src/main/kotlin/media/bcc/bccm_player/players/chromecast/CastExpandedControlsActivity.kt
+++ android/src/main/kotlin/media/bcc/bccm_player/players/chromecast/CastExpandedControlsActivity.kt
@@ -3,7 +3,7 @@ package media.bcc.bccm_player.players.chromecast
 import android.view.Menu
 import com.google.android.gms.cast.framework.CastButtonFactory
 import com.google.android.gms.cast.framework.media.widget.ExpandedControllerActivity
-import com.google.android.gms.cast.framework.media.uicontroller.UIMediaController;
+// import com.google.android.gms.cast.framework.media.uicontroller.UIMediaController;
 
 import media.bcc.bccm_player.R
 
@@ -13,9 +13,9 @@ class CastExpandedControlsActivity : ExpandedControllerActivity() {
         menuInflater.inflate(R.menu.cast_expanded_controller, menu)
         CastButtonFactory.setUpMediaRouteButton(this, menu, R.id.media_route_menu_item)
         
-        UIMediaController uiMediaController = getUIMediaController()
-        uiMediaController.bindViewToRewind(button_2, 10000)
-        uiMediaController.bindViewToForward(button_4, 10000)
+       // UIMediaController uiMediaController = getUIMediaController()
+       // uiMediaController.bindViewToRewind(button_2, 10000)
+       // uiMediaController.bindViewToForward(button_4, 10000)
 
         return true
     }
